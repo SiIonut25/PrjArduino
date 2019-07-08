f7241ca400fb0527109c2961ff07503c90d4a54e
ionutz_craciunescu_gmail <ionutz.craciunescu@gmail.com>
2019-07-08 21:32:09 +0300

    aq resources

--- Arduino_WS/js/script.js
+++ Arduino_WS/js/script.js
@@ -4,26 +4,37 @@ $(document).ready(function(){
 });
 
 function AddBootstrapMenu(){
-	$("#divHeader").html(menuHtml);
-	
+	$("#divHeader").html(menuHtml + loginHtml);
+	$("#btnLogin").click(function(){
+		TryLogin();
+	});
+}
+
+function TryLogin(){
+	$.ajax({
+		method: "POST",
+		url: "/login",
+		data: { username: $("#defaultForm-email").val(), password: $("#defaultForm-pass").val() }
+	  })
+		.done(function( msg ) {
+		  alert( "Data Saved: " + msg );
+		});
 }
 
 
 
-
-
 var menuHtml = '<nav class="navbar navbar-expand-lg navbar-light bg-light">\
-	<a class="navbar-brand" href="#">Navbar</a>\
+	<a class="navbar-brand" href="#">Meniu</a>\
 	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">\
 	  <span class="navbar-toggler-icon"></span>\
 	</button>\
   	<div class="collapse navbar-collapse" id="navbarSupportedContent">\
 	  <ul class="navbar-nav mr-auto">\
 		<li class="nav-item active">\
-		  <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>\
+		  <a class="nav-link" href="/">Acasa<span class="sr-only">(current)</span></a>\
 		</li>\
 		<li class="nav-item">\
-		  <a class="nav-link" href="#">Link</a>\
+		  <a class="nav-link" href="" data-toggle="modal" data-target="#modalLoginForm">Login</a>\
 		</li>\
 		<li class="nav-item dropdown">\
 		  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">\
@@ -46,3 +57,31 @@ var menuHtml = '<nav class="navbar navbar-expand-lg navbar-light bg-light">\
 	  </form>\
 	</div>\
   </nav>';
+
+  var loginHtml = '<div class="modal fade" id="modalLoginForm" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">\
+		<div class="modal-dialog" role="document">\
+			<div class="modal-content">\
+			<div class="modal-header text-center">\
+				<h4 class="modal-title w-100 font-weight-bold">Sign in</h4>\
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">\
+				<span aria-hidden="true">&times;</span>\
+				</button>\
+			</div>\
+			<div class="modal-body mx-3">\
+				<div class="md-form mb-5">\
+				<i class="fas fa-envelope prefix grey-text"></i>\
+				<input type="email" id="defaultForm-email" class="form-control validate">\
+				<label data-error="wrong" data-success="right" for="defaultForm-email">Your email</label>\
+				</div>\
+				<div class="md-form mb-4">\
+				<i class="fas fa-lock prefix grey-text"></i>\
+				<input type="password" id="defaultForm-pass" class="form-control validate">\
+				<label data-error="wrong" data-success="right" for="defaultForm-pass">Your password</label>\
+				</div>\
+			</div>\
+			<div class="modal-footer d-flex justify-content-center">\
+				<button class="btn btn-default id="btnLogin">Login</button>\
+			</div>\
+			</div>\
+		</div>\
+	</div>';
